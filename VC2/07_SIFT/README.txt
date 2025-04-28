3e86be777500a924392cb67dbba4bf117803b306
michamettler <mettlermicha@gmail.com>
2025-04-28 19:00:52 +0200

    init lab 7

---
+++ VC2/07_SIFT/README.txt
@@ -0,0 +1,20 @@
+=== SIFT FEATURE MATCHING ===
+
+Work through a typical natural feature matching pipeline and document 
+  - Steps,
+  - Results, and 
+  - Insights
+in a report (“Laborbericht”) using
+  - Text
+  - Code fragments (if applicable), and 
+  - Screenshots!
+
+The report, including figures (screenshots, etc.), might be around 2-4 pages (A4 format). Keep it short and to the point! Please upload the report as PDF file when done. Please do not upload any additional files, everything should be in the report!
+
+Almost all code is provided. First, make yourself familiar with the code (most parts should be rather self-explaining; for some parts you might need to look into the documentation -- i.e., google it!). Feel free to modify and/ or add some additional code to perform more detailed experiments and gain additional insights! 
+
+Task: record your own images and perform keypoint extraction, description and matching!
+
+**Play around with it!** Try to understand what's going on and ask yourself questions, such as: When does it work, what are limitations? Scale, Rotation,... Failure Cases?
+
+HAVE FUN!
---
+++ VC2/07_SIFT/SIFT_demo.py
@@ -0,0 +1,42 @@
+# IMPOTANT: as SIFT is patetned you have to install an older version:
+# pip install opencv-contrib-python==3.4.2.16
+
+#see: https://docs.opencv.org/master/db/d27/tutorial_py_table_of_contents_feature2d.html
+#especially sections: Introduction to SIFT (Scale-Invariant Feature Transform) and Feature Matching
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+img1 = cv.imread('scene.pgm',cv.IMREAD_GRAYSCALE)   # queryImage
+img2 = cv.imread('basmati.pgm',cv.IMREAD_GRAYSCALE) # trainImage
+
+# Initiate SIFT detector
+sift = cv.xfeatures2d.SIFT_create()
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+# display the matches
+img1M=cv.drawKeypoints(img1,kp1,img1,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+img2M=cv.drawKeypoints(img2,kp2,img2,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+plt.figure("Scene Keypoints")
+plt.axis('off')
+plt.imshow(img1M)
+plt.figure("Object Keypoints")
+plt.axis('off')
+plt.imshow(img2M)
+
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+
+# cv.drawMatchesKnn expects list of lists as matches.
+imgMatches = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.figure("Matches")
+plt.axis('off')
+plt.imshow(imgMatches),plt.show()
